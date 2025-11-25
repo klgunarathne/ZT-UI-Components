5fd8eda3da759b1b36b3a262791ae6fe9cf6f8ef
klgunarathne <k.l.gunarathne1988@gmail.com>
2025-11-25 19:33:01 +0530

    feat: export components from public API to fix build issues

--- projects/zt-ui-components/src/public-api.ts
+++ projects/zt-ui-components/src/public-api.ts
@@ -4,7 +4,23 @@
 
 export * from './lib/zt-ui-components';
 
-// Component modules with specific paths
+// Component exports (standalone components)
+export * from './lib/zt-button/button.component';
+export * from './lib/zt-card/card.component';
+export * from './lib/zt-input/input.component';
+export * from './lib/zt-modal/zt-modal.component';
+export * from './lib/zt-select/select.component';
+export * from './lib/zt-textarea/textarea.component';
+export * from './lib/zt-toggle/toggle.component';
+export * from './lib/zt-datetime-picker/datetime-picker.component';
+export * from './lib/zt-checkbox/checkbox.component';
+export * from './lib/zt-radio/radio.component';
+export * from './lib/zt-paginator/zt-paginator.component';
+export * from './lib/zt-paginator/zt-paginator-x/zt-paginator-x.component';
+export * from './lib/zt-data-grid/zt-data-grid.component';
+export * from './lib/zt-toast/zt-toast.component';
+
+// Component modules with specific paths (deprecated)
 export * from './lib/ui/button';
 export * from './lib/ui/card';
 export * from './lib/ui/input';
